7bb9d4aae23398341fce9ed26ec315d01119a028
Jerry <jlairs5@wgu.edu>
2023-08-05 08:49:01 -0400

    part D1: creates dockerfile

--- src/main/java/edu/wgu/d387_sample_code/config/WebConfig.java
+++ src/main/java/edu/wgu/d387_sample_code/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Allow requests from this origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
-                .allowedHeaders("*"); // Allow all headers
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
